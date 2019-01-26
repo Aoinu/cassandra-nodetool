dceef382c7ed8d78e0ead6cb6e5be39702a49c92
Aoinu <Aoinu607@gmail.com>
2019-01-27 06:23:15 +0900

    Exports

--- index.js
+++ index.js
@@ -2,6 +2,12 @@ const {
     JMXClient,
     JMXClientFactory,
 } = require('./lib/jmx-client');
+const {
+    Nodetool,
+    NodetoolCLI,
+} = require('./lib/nodetool');
 
 exports.JMXClient = JMXClient;
 exports.JMXClientFactory = JMXClientFactory;
+exports.Nodetool = Nodetool;
+exports.NodetoolCLI = NodetoolCLI;
